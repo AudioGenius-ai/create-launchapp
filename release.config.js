5599f2d5966fafd644af2f61d3e0aaac4055ffc1
Sami Shukri <samiziiv@gmail.com>
2025-05-31 16:42:51 -0600

    chore: add CHANGELOG and update release config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   git: {
     tagName: 'v${version}',
-    commitMessage: 'chore: release ${version}'
+    commitMessage: 'chore: release ${version}',
+    addFiles: ['CHANGELOG.md']
   },
   npm: {
     publish: false
   },
   plugins: {
     '@release-it/conventional-changelog': {
-      preset: 'conventionalcommits'
+      preset: 'conventionalcommits',
+      infile: 'CHANGELOG.md'
     }
   },
   hooks: {
